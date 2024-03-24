0afae4fb60d76b8806c3e53c949d810616d831fb
Anatoliy Odinetskiy <aodinetsky@yandex-team.ru>
2024-03-24 14:46:06 +0700

    Initial Commit

---
+++ L9/L9App.swift
@@ -0,0 +1,17 @@
+//
+//  L9App.swift
+//  L9
+//
+//  Created by Anatoliy Odinetskiy on 24.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct L9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
